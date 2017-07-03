e5fcb627c1eb4aab483589f7aa777845d1502a63
Fellipe Caetano <fellipe@MacBook-Pro-de-Sympla-2.local>
2017-07-02 21:31:13 -0300

    Initial Commit

---
+++ Democracy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Democracy
+//
+//  Created by Fellipe Caetano on 02/07/17.
+//  Copyright © 2017 TDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
