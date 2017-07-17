331d3836b792b98ecf12f2cd796e2d7df76452d7
Fellipe Caetano <fellipe.caetano4@gmail.com>
2017-07-16 23:38:33 -0300

    Remove type parameter from Request

--- Sources/Application/Networking/APIDefaults.swift
+++ Sources/Application/Networking/APIDefaults.swift
@@ -3,7 +3,7 @@ import Foundation
 struct APIDefaults {
     private static let baseURL = URL(string: "http://localhost:9000")!
 
-    static func enhanceRequest<T>(_ request: Request<T>) -> Request<T> {
+    static func enhanceRequest(_ request: Request) -> Request {
         guard var components = URLComponents(url: request.url, resolvingAgainstBaseURL: true) else {
             fatalError("Attempt to enhance malformed Request: \(request)")
         }
@@ -12,7 +12,7 @@ struct APIDefaults {
         components.host = baseURL.host
         components.port = baseURL.port
 
-        return Request<T>(
+        return Request(
             url: components.url!,
             method: request.method,
             parameters: request.parameters,
